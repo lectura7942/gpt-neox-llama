a65b2c30446586a1ae5a51bf8bc2fd3ee4ce0639
권지은_T5018 <81620001+lectura7942@users.noreply.github.com>
2023-10-05 10:01:03 +0900

    Add preprocess_data script with HFLlamaTokenizer

---
+++ scripts/preprocess_data.sh
@@ -0,0 +1,7 @@
+python tools/datasets/preprocess_data.py \
+    --input ./data/news_data_en_short.jsonl \
+    --jsonl-keys text \
+    --output-prefix ./data/pretrain_dataset \
+    --tokenizer-type HFLlamaTokenizer \
+    --append-eod \
+    --workers 16
